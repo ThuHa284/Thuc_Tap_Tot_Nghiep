c7429f5752341ba3e2af47baa20d86ddb9b905c3
Kit <tak1632004@gmail.com>
2026-05-08 01:10:41 +0700

    update: add new module QuanLyUser and module KhaiBaoNgoaiTru

---
+++ Modules/KhaiBaoNgoaiTru/resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.master')
+
+@section('title', 'Quản lý Người dùng - Admin')
+
+@section('content')
+Hiện tại module Khai báo ngoại trú đang trong quá trình phát triển. Vui lòng quay lại sau!
+@endsection
